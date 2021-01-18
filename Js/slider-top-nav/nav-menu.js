5ddbde0ef769483ca1a4389697cc21e2e2673a36
Wildan <muhwildanjaffar@gmail.com>
2021-01-18 16:16:50 +0700

    Update

--- Js/slider-top-nav/nav-menu.js
+++ Js/slider-top-nav/nav-menu.js
@@ -191,6 +191,10 @@ if (typeof jQuery === 'undefined') {
             }
         });
 
+        $('.' + settings.class_content + ' > li').on("contextmenu", function () {
+            return false;
+        });
+
         $('.' + settings.class_content).on('scroll', function () {
             enable_btn();
         });
